e28e7568e65703e8239874896919f9eda2f68350
pezzbear <edanzdeno@gmail.com>
2022-04-08 13:22:09 -0500

    Finished setup for Battlefield and Controller Classes

--- BattleShip/Battlefield.cs
+++ BattleShip/Battlefield.cs
@@ -1,8 +1,54 @@
-﻿namespace BattleShip {
+﻿//-----------------------------------------------------------------------
+// <copyright file="Battlefield.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace BattleShip 
+{
     using System;
     using System.Collections.Generic;
     using System.Text;
-    class Battlefield {
-        public int size;
+
+    /// <summary>
+    /// Class used to track grid data for the battlefield board.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public class Battlefield 
+    {
+        /// <summary>
+        /// Size of the battlefield grid
+        /// </summary>
+        public int Size;
+
+        /// <summary>
+        /// A 2d array of enum values that will be used to track what's happening during gameplay.
+        /// </summary>
+        public GridData[,] DataGrid;
+
+        /// <summary>
+        /// Size of the battlefield grid
+        /// </summary>
+        /// public Ship[] ShipGrid; <--- ***UNCOMMENT THIS WHEN MERGED***
+
+        /// <summary>
+        /// Data entered in the battlefield grids.
+        /// </summary>
+        public enum GridData 
+        {
+            /// <summary>
+            /// Empty space on the board
+            /// </summary>
+            Empty,
+
+            /// <summary>
+            /// Represents when a player misses a ship after firing
+            /// </summary>
+            Miss,
+
+            /// <summary>
+            /// Represents when a player hits a ship after firing
+            /// </summary>
+            Hit
+        }
     }
 }
