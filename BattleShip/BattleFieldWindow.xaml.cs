55ffcee82dab381a67c41799c45dc40421dd5a73
chickentree9 <jaklinrobert@gmail.com>
2022-04-10 19:40:40 -0500

    Grid System Layout Nothing Yet on the BattleField Window besides layout

---
+++ BattleShip/BattleFieldWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file=this.file company="BattleShip Team">
+// </copyright>
+// <author>Robert,Alberto,Trik,Edan</author>
+namespace BattleShip
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for BattleFieldWindow.xaml
+    /// </summary>
+    public partial class BattleFieldWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BattleFieldWindow"/> class
+        /// </summary>
+        public BattleFieldWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
